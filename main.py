cbe3907f2fe72cb39a08e0060a7c8ecd0ad023ed
Ujjwal Garai <ujjwalgarai6@gmail.com>
2026-07-05 17:32:31 +0530

    CRUD operation initiate

--- main.py
+++ main.py
@@ -5,37 +5,87 @@ app = FastAPI()
 
 users = [
     {
-        "Name":"Ujjwal",
+        "id": 1,
+        "Name": "Ujjwal",
         "Age": 30
     },
     {
-        "Name":"Sampa",
+        "id": 2,
+        "Name": "Sampa",
         "Age": 30
     },
     {
-        "Name":"Tiyasa",
+        "id": 3,
+        "Name": "Tiyasa",
         "Age": 2
     },
     {
-        "Name":"Jaydeb",
+        "id": 4,
+        "Name": "Jaydeb",
         "Age": 59
     },
     {
-        "Name":"Rekha",
+        "id": 5,
+        "Name": "Rekha",
         "Age": 52
     },
- 
-    ]
 
-class Address(BaseModel):
-    city: str
-    pin:int
+]
+
 
 class User(BaseModel):
-    name: str
-    age:int
-    address:Address
+    id: int
+    Name: str
+    Age: int
+
 
 @app.post("/user")
-def create_User(user: User):
-    return user
+def createUser(user: User):
+    users.append(user)
+    return {
+        "Message": "New User Data Created",
+        "Data": user
+    }
+
+
+@app.get("/users")
+def getUsers():
+    return users
+
+
+@app.get("/user/{user_id}")
+def getUser(user_id: int):
+    for user in users:
+        if user["id"] == user_id:
+            return {
+                "Message": "New User Data Created",
+                "Data": user
+            }
+    return {
+        "Message": "User not found",
+    }
+
+@app.put("/userModification/{user_id}")
+def userModification(user_id: int, updatedUser:User):
+    for index, user in enumerate(users):
+        if user["id"] == user_id:
+            users[index] = updatedUser
+            return {
+                "Message": "User Data modified",
+                "Data": updatedUser
+            }
+    return {
+        "Message": "User not found",
+    }
+
+@app.delete("/user/{user_id}")
+def delete_user(user_id:int):
+    for index, user in enumerate(users):
+        if user["id"] == user_id:
+            users.pop(index)
+            return {
+                "Message": "User deleted",
+            }
+    return {
+                "Message": "User not found",
+            }
