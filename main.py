90007f4449a8a516a1c9174f30aad12e3b4b5432
Ujjwal Garai <ujjwalgarai6@gmail.com>
2026-07-24 09:12:50 +0530

    sqlite3 code add

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI, Request
-from time import time
+from fastapi import FastAPI
+import sqlite3
+app = FastAPI(title="SQLite3 Learning")
 
-app = FastAPI()
 
-@app.middleware("http")
-async def timeTracking(req: Request, call_next):
-    start_time = time()
-    print(start_time)
-    response = await call_next(req)
-    processTime = time() - start_time
-    print (f"Path {req.url.path} \ntime taken: {processTime}")
-    return response
+conn = sqlite3.connect("test.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS todos(
+               id INTEGER PRIMARY KEY,
+               TITLE TEXT,
+               STATUS TEXT
+               )
+""")
+conn.commit()
